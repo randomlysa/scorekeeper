396318a4a7d8dc283cc739c0caa69b9a7c56f693
randomlysa <su@randomlysa.com>
2018-11-08 16:01:16 -0600

    style Player, AddPlayer, and the form

--- imports/ui/Player.js
+++ imports/ui/Player.js
@@ -16,17 +16,24 @@ export default class Player extends React.Component{
   render() {
     return (
       <div key={this.props.player._id} className="item">
-        <p>
-          {this.props.player.name} has {this.props.player.score} point(s)
-        </p>
+        <div className="player">
+          <div>
+            <h3 className="player__name">{this.props.player.name}</h3>
+            <p className="player__stats">
+              {this.props.player.score} point(s)
+            </p>
+          </div>
 
-        <button className="button button--round" onClick={this.modScore.bind(this, this.props.player._id, 1)}>+1</button>
-        <button className="button button--round" onClick={this.modScore.bind(this, this.props.player._id, -1)}>-1</button>
-        <button className="button button--round" onClick={
-          () => {
-            Players.remove({_id: this.props.player._id})
-          }
-        }> X </button>
+          <div className="player__actions">
+            <button className="button button--round" onClick={this.modScore.bind(this, this.props.player._id, 1)}>+1</button>
+            <button className="button button--round" onClick={this.modScore.bind(this, this.props.player._id, -1)}>-1</button>
+            <button className="button button--round" onClick={
+              () => {
+                Players.remove({_id: this.props.player._id})
+              }
+            }> X </button>
+          </div>
+        </div>
       </div>
     )
   }
