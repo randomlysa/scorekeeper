58e5ea6f83d5b9b7f3d696b2bed6a3c69117ee7a
randomlysa <su@randomlysa.com>
2018-11-07 21:15:15 -0600

    insert players into mongodb and display using react

--- client/main.js
+++ client/main.js
@@ -1,39 +1,29 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import { Meteor } from 'meteor/meteor'
+import { Tracker } from 'meteor/tracker'
 
-const players = [{
-  _id: '1',
-  name: 'Lauren',
-  score: 99
-}, {
-  _id: '2',
-  name: 'Jeff',
-  score: -1
-}, {
-  _id: '3',
-  name: 'Andrew',
-  score: -12
-}]
+import { Players } from '../imports/api/players'
 
 const renderPlayers = (playerList) => {
   return playerList.map(player => {
     return <p key={player._id}>{player.name} has {player.score} point(s).</p>
   })
-
 }
 
 Meteor.startup(() => {
-  const title = 'Score Keeper'
-  let jsx = (
-    <div>
-      <h1>{title}</h1>
-      {renderPlayers(players)}
-
-    </div>
-  )
-
-  ReactDom.render(
-    jsx, document.getElementById('app')
-  )
-})
+  Tracker.autorun(() => {
+    let players = Players.find().fetch()
+    const title = 'Score Keeper'
+    let jsx = (
+      <div>
+        <h1>{title}</h1>
+        {renderPlayers(players)}
+      </div>
+    ); // let jsx
+    ReactDom.render(
+      jsx,
+      document.getElementById('app')
+    ); // ReactDom.render
+  }); // Tracker.autorun
+}) // Meteor.startup
--- server/main.js
+++ server/main.js
@@ -1,1 +1,11 @@
-console.log('Log from /server/main.js')
+import { Meteor } from 'meteor/meteor'
+import { Players } from '../imports/api/players'
+
+Meteor.startup(() => {
+  Players.insert({
+    name: 'Dave',
+    score: 40
+  })
+
+  console.log(Players.find().fetch())
+})
