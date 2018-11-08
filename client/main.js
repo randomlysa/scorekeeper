e247b54f97f1518f33714db0a53e3d44ff11eb07
randomlysa <su@randomlysa.com>
2018-11-07 21:47:40 -0600

    add button that deletes a player

--- client/main.js
+++ client/main.js
@@ -7,7 +7,16 @@ import { Players } from '../imports/api/players'
 
 const renderPlayers = (playerList) => {
   return playerList.map(player => {
-    return <p key={player._id}>{player.name} has {player.score} point(s).</p>
+    return (
+      <p key={player._id}>{
+        player.name} has {player.score} point(s).
+        <button onClick={
+          () => {
+            Players.remove({_id: player._id})
+          }
+        }> X </button>
+      </p>
+    ) // return
   })
 }
 
