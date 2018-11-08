77ad2fc74748ba58e0489826dade003efeb3a973
randomlysa <su@randomlysa.com>
2018-11-08 15:02:33 -0600

    starting some scss styling

--- imports/ui/TitleBar.js
+++ imports/ui/TitleBar.js
@@ -4,8 +4,10 @@ import PropTypes from 'prop-types'; // ES6
 export default class TitleBar extends React.Component{
   render() {
     return (
-      <div>
-        <h1>{this.props.title}</h1>
+      <div className="title-bar">
+        <div className="wrapper">
+          <h1>{this.props.title}</h1>
+        </div>
       </div>
     );
   } // render()
