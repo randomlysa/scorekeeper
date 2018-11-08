3b98b903b8c9c5b2589b77065923d10037558787
randomlysa <su@randomlysa.com>
2018-11-07 21:25:43 -0600

    remove player insert on startup

--- server/main.js
+++ server/main.js
@@ -2,10 +2,5 @@ import { Meteor } from 'meteor/meteor'
 import { Players } from '../imports/api/players'
 
 Meteor.startup(() => {
-  Players.insert({
-    name: 'Dave',
-    score: 40
-  })
 
-  console.log(Players.find().fetch())
 })
