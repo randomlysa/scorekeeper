331054f7cc0c804eb799f48fdc75095a5cfe4515
randomlysa <su@randomlysa.com>
2018-11-08 19:51:38 -0600

    different styles for 1-3rd place

--- imports/ui/Player.js
+++ imports/ui/Player.js
@@ -14,13 +14,14 @@ export default class Player extends React.Component{
   } // modScore
 
   render() {
+    let itemClassName = `item item--position-${this.props.player.rank}`
     return (
-      <div key={this.props.player._id} className="item">
+      <div key={this.props.player._id} className={itemClassName}>
         <div className="player">
           <div>
             <h3 className="player__name">{this.props.player.name}</h3>
             <p className="player__stats">
-              {this.props.player.rank} {this.props.player.position} {this.props.player.score} point(s)
+              {this.props.player.position} place - {this.props.player.score} point(s)
             </p>
           </div>
 
